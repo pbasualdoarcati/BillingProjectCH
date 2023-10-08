a0170fc4226293168d8f3a7aa5b5007f3b3bbe4e
pablo <pbasualdoarcati@hotmail.com>
2023-10-08 20:52:23 -0300

    Fix: Remove redundant import

--- src/main/java/com/billing/finalproject/controller/InvoiceDetailsController.java
+++ src/main/java/com/billing/finalproject/controller/InvoiceDetailsController.java
@@ -1,20 +1,10 @@
 package com.billing.finalproject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.billing.finalproject.entity.Invoice;
 import com.billing.finalproject.entity.InvoiceDetails;
-import com.billing.finalproject.entity.Product;
 import com.billing.finalproject.service.InvoiceDetailsService;
-import com.billing.finalproject.service.InvoiceService;
-import com.billing.finalproject.service.ProductService;
 
 import java.util.Optional;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/billing/finalproject/controller/ProductController.java
+++ src/main/java/com/billing/finalproject/controller/ProductController.java
@@ -1,14 +1,8 @@
 package com.billing.finalproject.controller;
 
-import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.billing.finalproject.entity.Product;
 import com.billing.finalproject.service.ProductService;
